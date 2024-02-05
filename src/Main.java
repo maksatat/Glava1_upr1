687a0130676a5a296ed134265078f2831658d51e
maksatat <159049081+maksatat@users.noreply.github.com>
2024-02-05 22:16:34 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/*
+Cила тяжести на Луне составляет около 17% земной. Напишите программу
+для расчета вашего фактического веса на Луне.
+
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        double zemlya;
+        zemlya = 110;
+        System.out.println("Мой вес на луне равен " + (zemlya * 0.17));
+        }
+    }
